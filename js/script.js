5b865f242fd180b67cf8ab8d1d83ffc49b6bb378
fed <fed_v@hotmail.com>
2016-06-12 10:58:06 -0400

    Added a faux console for debugging

--- js/script.js
+++ js/script.js
@@ -1,6 +1,25 @@
-window.onload = function(e) {
-    alert("window loaded");     
+window.onload = function(e) {    
+        
+    var console = {
+        log: function(txt, type='text') {
+                 var para = document.createElement('p');
+                 var node = document.createTextNode(txt);
+                 para.appendChild(node);
+                 
+                 if(type == 'error') {
+                     para.setAttribute("class", "error");
+                 }
+    
+                 var element = document.getElementById("console");
+                 element.appendChild(para);   
+             }
+    };
+    
+    console.log("test");
+    console.log("boobs", "error");
+      
 }
+
 
 // jQuery onload function
 $(document).ready(function() {
